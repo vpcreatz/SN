adc8cd502e129458f373c852c13012f4a0775628
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-27 21:40:45 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -22,7 +22,7 @@ from .helper.telegram_helper.message_utils import sendMessage, editMessage, send
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
 from bot import config_dict, botStartTime, Interval, QbInterval, LOGGER, DATABASE_URL, bot, dispatcher, updater, IGNORE_PENDING_REQUESTS, \
-                app, main_loop, user_data
+                app, main_loop, alive, user_data
 from .modules import authorize, list, cancel_mirror, mirror_status, mirror_leech, clone, ytdlp, shell, eval, bot_settings, \
                      delete, count, users_settings, search, rss, wayback, speedtest, anilist, imdb, bt_select, mediainfo, hash, \
                      scraper, pictures, save_msg, sel_cat, users, drive_clean, broadcast
@@ -144,6 +144,7 @@ def restart(update, context):
     if QbInterval:
         QbInterval[0].cancel()
         QbInterval.clear()
+    alive.kill()
     clean_all()
     srun(["pkill", "-9", "-f", "gunicorn|aria2c|qbittorrent-nox|ffmpeg"])
     srun(["python3", "update.py"])
